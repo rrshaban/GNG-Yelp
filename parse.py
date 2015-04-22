febb7295a877983186d13ad656b56bf31b6a669c
Razi <rrshaban@users.noreply.github.com>
2015-04-22 14:38:10 -0400

    list 20 most populated (by business) cities in dataset

--- parse.py
+++ parse.py
@@ -1,1 +1,16 @@
 import json
+import operator
+
+b = list()
+for line in open('data/yelp_academic_dataset_business.json'):
+  b.append(json.loads(line))
+
+c = dict()
+for bus in b:
+  if bus['city'] in c:
+    c[bus['city']] += 1
+  else:
+    c[bus['city']] = 1
+sorted_c = sorted(c.items(), key=operator.itemgetter(1), reverse=True)
+
+print(sorted_c[:20])
